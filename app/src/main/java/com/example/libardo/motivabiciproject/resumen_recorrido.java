5e93dbf3fa2fefb3ccfdfba37736d67bcc47f08a
libardomm <libardoxp@gmail.com>
2016-03-24 23:44:21 -0500

    implementacion de vista de resumen y de metodo altitudMaxima

---
+++ app/src/main/java/com/example/libardo/motivabiciproject/resumen_recorrido.java
@@ -0,0 +1,13 @@
+package com.example.libardo.motivabiciproject;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class resumen_recorrido extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumen_recorrido);
+    }
+}
--- app/src/main/java/com/example/libardo/motivabiciproject/ubicacion.java
+++ app/src/main/java/com/example/libardo/motivabiciproject/ubicacion.java
@@ -1,6 +1,7 @@
 package com.example.libardo.motivabiciproject;
 
 import android.content.Context;
+import android.content.Intent;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
@@ -14,6 +15,10 @@ import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ubicacion extends ActionBarActivity implements LocationListener {
   LocationManager locationManager;
@@ -37,27 +42,26 @@ public class ubicacion extends ActionBarActivity implements LocationListener {
     Float distanciaEnMetros;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ubicacion);
         //botones y textViews
-        botonEmpezar=(Button)findViewById(R.id.bttIniciar);
-        botonTerminar=(Button)findViewById(R.id.bttTerminar);
-        botonDistancia=(Button)findViewById(R.id.bttDistancia);
-        botonRestablecer=(Button)findViewById(R.id.bttRestablecer);
-        botonvelocidad=(Button)findViewById(R.id.bttVelocidadMedia);
+        botonEmpezar = (Button) findViewById(R.id.bttIniciar);
+        botonTerminar = (Button) findViewById(R.id.bttTerminar);
+        botonDistancia = (Button) findViewById(R.id.bttDistancia);
+        botonRestablecer = (Button) findViewById(R.id.bttRestablecer);
+        botonvelocidad = (Button) findViewById(R.id.bttVelocidadMedia);
 
         //ubicacion
-        locationManager=(LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        provider=locationManager.getBestProvider(new Criteria(),false);
-        Location location=locationManager.getLastKnownLocation(provider);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(), false);
+        Location location = locationManager.getLastKnownLocation(provider);
 
-        if(location!=null){
-            Toast.makeText(getApplicationContext(),"Si hay ubicacion",Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(getApplicationContext(),"Si hay ubicacion",Toast.LENGTH_SHORT).show();
+        if (location != null) {
+            Toast.makeText(getApplicationContext(), "Si hay ubicacion", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Si hay ubicacion", Toast.LENGTH_SHORT).show();
 
         }
         //tiempo
@@ -69,6 +73,12 @@ public class ubicacion extends ActionBarActivity implements LocationListener {
         botonRestablecer.setEnabled(false);
         botonDistancia.setEnabled(false);
         botonvelocidad.setEnabled(false);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ubicacion.this, resumen_recorrido.class));
+            }
+        });
 
     }
     //metodos adicionaeles
@@ -191,6 +201,13 @@ public void empezarRecorrido(View v){
         Toast.makeText(getApplicationContext(),"Calorias quemadas "+calorias.toString(),Toast.LENGTH_SHORT).show();
 
     }
+public void maximaAltitud(){
+    location2=locationManager.getLastKnownLocation(provider);
+    cadenalocation1= String.valueOf(location2.getAltitude());
+    Toast.makeText(getApplicationContext(),cadenalocation2,Toast.LENGTH_SHORT).show();
 
+
+
+}
 
 }
